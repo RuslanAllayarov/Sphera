915f9a7cd656cd8a65039cb8e7be77fa537a0863
RuslanAllayarov <47866101+RuslanAllayarov@users.noreply.github.com>
2019-04-03 23:10:46 +0300

    Create Matrix.h

---
+++ msu_spring_2019/03/Matrix.h
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <stdexcept>
+
+class Row
+{
+    size_t length; // длина строки
+    int* data_row;
+public:
+    Row(int * begin_add, size_t len) :length(len), 
+                                        data_row (begin_add) { }
+    
+    int& operator[] (size_t x)
+    {
+        if ((x < length) && (x >= 0)) // то есть корректный запрос
+        {
+            return data_row[x];
+        }
+        else
+        {
+            throw std::runtime_error("Uncorrect a[i]");
+        }
+    }
+
+    int& operator[] (size_t x) const
+    {
+        if ((x < length) && (x >= 0))
+        {
+            return data_row[x];
+        }
+        else
+        {
+            throw std::runtime_error("Uncorrect a[i]");
+        }
+    }
+
+    ~Row() { }
+};
+
+
+class Matrix
+{
+    size_t rows;
+    size_t columns;
+    int* data_matrix;
+public:
+    Matrix(const size_t rows, const size_t columns) : rows(rows),columns(columns),
+                                                      data_matrix (new int[rows*columns]){
+        for(size_t d = 0; d < rows*columns; d++)
+        {
+            data_matrix[d] = 0;
+        }
+    }
+
+    size_t getRows() const
+    {
+        return rows;
+    }
+
+    size_t getColumns() const
+    {
+        return columns;
+    }
+
+    const Row operator[](size_t i) const
+    {
+        if ((i >= 0) && (i < rows))
+        {
+            Row res = Row(&data_matrix[i*columns], columns);
+            return res;
+        }
+        else
+        {
+            throw std::runtime_error("a[.][]");
+        }
+    }
+
+    Row operator[](size_t i)
+    {
+        if ((i >= 0) && (i < rows))
+        {
+            Row res = Row(&data_matrix[i*columns], columns);
+            return res;
+        }
+        else
+        {
+            throw std::runtime_error("a[.][]");
+        }
+    }
+
+    Matrix& operator*=(size_t m)
+    {
+        for (size_t i = 0; i < rows*columns; i++)
+            data_matrix[i] *= m;
+        return *this;
+    }
+
+    bool operator==(const Matrix& other) const
+    {
+        if ((rows != other.rows) || (columns != other.columns))
+        {
+            return false;
+        }
+        for (size_t i = 0; i < rows*columns; i++)
+        {
+            if (data_matrix[i] != other.data_matrix[i])
+                return false;
+        }
+        return true;
+    }
+
+    bool operator!=(const Matrix& other) const
+    {
+        return !(*this == other);
+    }
+
+    ~Matrix()
+    {
+        delete[] data;
+    }
+};
