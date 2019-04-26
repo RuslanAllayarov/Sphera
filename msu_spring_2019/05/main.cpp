bbbba752520e1191a67ba185e31e165d0416e63e
RuslanAllayarov <47866101+RuslanAllayarov@users.noreply.github.com>
2019-04-26 17:28:01 +0300

    Changed the structure of the function

--- msu_spring_2019/05/main.cpp
+++ msu_spring_2019/05/main.cpp
@@ -1,27 +1,27 @@
 #include <thread>
 #include <iostream>
 #include <atomic>
+#include <mutex>
+#include <condition_variable>
 
+std::condition_variable cv;
+std::mutex mutex;
 static const size_t MAX = 1000000;
 static std::atomic_int64_t j{0};
 
 
 void threadFunction(size_t ind)
 {
-    while (true)
+    const char* str = ind ? "ping" : "pong";
+    while (counter < MAX - 1)
     {
-        if (j >= MAX)
-            break;
-        if ((ind == 0) && (j % 2 == 0))
-        {
-            std::cout << "ping" << std::endl;
-            j++;
-        }
-        if ((ind == 1) && (j % 2 == 1))
-        {
-            std::cout << "pong" << std::endl;
-            j++;
-        }
+        std::unique_lock<std::mutex> locker(mutex);
+        cv.wait(locker, [ind]{
+            return ((ind == 0) && (counter % 2 == 1)) || ((ind == 1) && (counter % 2 == 0));
+            });
+        std::cout << str << " " << MAX - counter << std::endl;
+        counter++;
+        cv.notify_one();
     }
 } 
 
