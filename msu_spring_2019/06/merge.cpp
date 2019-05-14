2c2bebcd0023a3c762c8b9f588c86e5364b9e985
RuslanAllayarov <47866101+RuslanAllayarov@users.noreply.github.com>
2019-05-14 23:47:46 +0300

    Update opening file

--- msu_spring_2019/06/merge.cpp
+++ msu_spring_2019/06/merge.cpp
@@ -20,6 +20,7 @@ void Separation(const char* name, uint64_t kol)
     uint64_t count_sorted = N;
     ifstream f;
     ofstream f1, f2;
+    bool is_not_open = false;
     while (count_sorted < kol) 
     {
         while (!dataReady)
@@ -29,6 +30,12 @@ void Separation(const char* name, uint64_t kol)
 	f.open(name, ios::binary);
 	f1.open("sort_left", ios::binary);
 	f2.open("sort_right", ios::binary);
+	if (!f1.is_open() || !f2.is_open() || !f.is_open())
+	{
+	    cout << "Error at opening file in Separation" << endl;
+	    is_not_open = true;
+	    break; 
+	}
 	if (!f.eof())  f.read(reinterpret_cast<char*>(&tmp), sizeof(uint64_t));
 	while (!f.eof()) 
 	{
@@ -50,9 +57,12 @@ void Separation(const char* name, uint64_t kol)
 	count_sorted *= 2;
 	ready.notify_one();
     }
-    f.close();
-    f1.close();
-    f2.close();
+    if(!is_not_open)
+    {
+	f2.close();
+	f1.close();
+	f.close();
+    }
 }
 
 
@@ -63,6 +73,7 @@ void Merge(const char* name, uint64_t kol)
     uint64_t count_sorted = N;
     ifstream f1, f2;
     ofstream f;
+    bool is_not_open = false;
     while (count_sorted < kol) 
     {
 	while (dataReady)
@@ -70,6 +81,12 @@ void Merge(const char* name, uint64_t kol)
 	f.open(name, ios::binary);
 	f1.open("sort_left", ios::binary);
 	f2.open("sort_right", ios::binary);
+	if (!f1.is_open() || !f2.is_open() || !f.is_open())
+	{
+	    cout << "Error at opening file in Merge" << endl;
+	    is_not_open = true;
+	    break;
+	}
 	if (!f1.eof()) f1.read(reinterpret_cast<char*>(&left), sizeof(uint64_t)); else break;
 	if (!f2.eof()) f2.read(reinterpret_cast<char*>(&right), sizeof(uint64_t)); else break;
 	while (!f1.eof() && !f2.eof()) 
@@ -121,12 +138,15 @@ void Merge(const char* name, uint64_t kol)
 	dataReady = true;
 	ready.notify_one();
     }
-    f2.close();
-    f1.close();
-    f.close();
+    if(!is_not_open)
+    {
+	f2.close();
+	f1.close();
+	f.close();
+	remove("sort_left");
+	remove("ssort_right");
+    }
     complete = true;
-    remove("sort_left");
-    remove("ssort_right");
     ready.notify_one();
 }
 
@@ -136,8 +156,13 @@ int BatchSort(const char* name)
     ofstream f2;
     f1.open(name, ios::binary);
     f2.open("data_help.dat", ios::binary);
-    if (f1.eof())
+    if (!f1.is_open() || !f2.is_open())
+    {
+	cout << "Error at open file in BatchSort" << endl;
 	return -1;
+    }
+    if (f1.eof())
+	return -2;
     uint64_t  Sorted_Numbers = 0; // количество отсортированных чисел
     uint64_t* buf = new uint64_t[N];
     while (!f1.eof())
